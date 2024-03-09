82b8b4ef4ea18ee2e27884f65506c7f73e0db386
matematiflo <7537413+matematiflo@users.noreply.github.com>
2024-03-09 14:30:16 -0500

    Cleanup

--- MathlibVersions/DefsWithSimpTag.lean
+++ MathlibVersions/DefsWithSimpTag.lean
@@ -10,9 +10,7 @@ import Mathlib.Algebra.Ring.Defs
 import Mathlib.Data.List.BigOperators.Basic
 import Mathlib.Data.Rat.Defs
 
--- Some things should be added to `simp`, it will make the proofs shorter
-
-namespace MathlibSumSq2
+namespace MathlibSumSq
 
 universe u
 variable {R : Type u}
@@ -37,24 +35,25 @@ def SumSqTR [Add R] [Zero R] [Pow R ℕ] : List R → R
 theorem SumSqAuxZero [Add R] [Zero R] [Pow R ℕ] (L : List R) : SumSqAux 0 L = SumSqAux (SumSq []) L := by rfl
 
 /-
-We now want to prove that the tail-recursuve definition agree with the original one: `∀ L : List R, SumSqTR L = SumSq L`. The key is that, when `S = SumSq L2`, the term  `SumSqAux S L1` can be computed in terms of `SumSq L1` and `SumSq L2`.
+We now want to prove that the tail-recursive definition agree with the original one: `∀ L : List R, SumSqTR L = SumSq L`. The key is that, when `S = SumSq L2`, the term  `SumSqAux S L1` can be computed in terms of `SumSq L1` and `SumSq L2`.
 -/
 theorem SumSqAuxWithSumSq [AddCommMonoid R] [Pow R ℕ] (L1 : List R) : ∀ L2 : List R, SumSqAux (SumSq L2) L1  = SumSq L2 + SumSq L1 := by
   induction L1 with
-  | nil => simp [SumSqAux]
-  | cons a l1 ih => intro L; simp [SumSqAux]; rw [add_comm _ (a ^2), ← SumSq,ih (a :: L), SumSq, add_comm (a ^ 2) _, add_assoc]
+  | nil => simp only [SumSqAux, SumSq]; intro L2; simp only [add_zero]
+  | cons a l1 ih => intro L; rw [SumSq, SumSqAux, add_comm _ (a ^2), ← SumSq,ih (a :: L), SumSq, add_comm (a ^ 2) _, add_assoc]
 
 -- We can now prove that `SumSqTR L = SumSq L`.
-lemma SumSqAuxEmptyList [AddCommMonoid R] [Pow R ℕ] (L : List R) : SumSqAux (SumSq []) L= SumSqAux (SumSq L) [] := by rw [SumSqAuxWithSumSq]; simp [SumSqAux]
+lemma SumSqAuxEmptyList [AddCommMonoid R] [Pow R ℕ] (L : List R) : SumSqAux (SumSq []) L= SumSqAux (SumSq L) [] := by simp only [SumSqAuxWithSumSq]; simp only [SumSq, zero_add, add_zero]
 
-theorem def_TR_ok [AddCommMonoid R] [Pow R ℕ] (L : List R) : SumSqTR L = SumSq L := by simp [SumSqAuxEmptyList, SumSqAux]
+theorem def_TR_ok [AddCommMonoid R] [Pow R ℕ] (L : List R) : SumSqTR L = SumSq L := by
+  simp only [SumSqTR, SumSqAuxZero, SumSqAuxEmptyList, SumSqAux]
 
 -- A sum-of-squares function on `List R` can also be defined as the composition of the function `L => (L.map (. ^ 2))` with `L => L.sum`. We show that the two definitions agree.
 theorem squaring_and_summing [AddCommMonoid R] [Pow R ℕ] (L : List R) : SumSq L = (L.map (. ^ 2)).sum := by
   induction L with
   | nil => rfl
-  | cons a l ih => simp [ih]
+  | cons a l ih => simp only [SumSq, List.map, List.sum_cons, ih]
 
-end MathlibSumSq2
+end MathlibSumSq
 
-#lint
+-- #lint

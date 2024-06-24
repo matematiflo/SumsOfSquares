3d32f893f935072928333d5b9bd572f4dec383ac
matematiflo <7537413+matematiflo@users.noreply.github.com>
2024-06-24 07:12:01 +0200

    chore: Update leanOptions in lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -3,6 +3,10 @@ open Lake DSL
 
 package SumSq where
   -- add package configuration options here
+leanOptions := #[
+    ⟨`pp.unicode.fun, true⟩, -- pretty-prints `fun a ↦ b`
+    ⟨`pp.proofs.withType, false⟩
+  ]
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
